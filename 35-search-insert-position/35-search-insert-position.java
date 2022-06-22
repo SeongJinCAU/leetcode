8e1239b4165771f71fcaa782e7ecd1f4899582ed
remope <39955801+remope@users.noreply.github.com>
2022-06-23 04:27:38 +0900

    Time: 0 ms (100.00%), Space: 44 MB (17.14%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static int searchInsert(int[] nums, int target) {
+        if(target<= nums[0]) return 0;
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+        while(start <= end){
+            mid = (start + end) / 2;
+            if(target == nums[mid]) return mid;
+            else if(target < nums[mid]) {
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        if(start > end) return end + 1;
+        return -1;
+    }
+}
