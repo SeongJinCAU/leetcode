26d8327cb98b034857d468a6566af0577800b87b
Choi Seong Jin <39955801+remope@users.noreply.github.com>
2022-08-02 20:34:45 +0900

    Time: 139 ms (44.95%), Space: 46.2 MB (14.86%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.ts
@@ -1,13 +1,8 @@
 function removeDuplicates(nums: number[]): number {
-    let count : number = 0;
-    let judge:boolean[] = new Array(201);
-    for(let i = nums.length - 1; i >= 0; i--){
-        if(judge[nums[i]+100]){
+    for(let i = nums.length - 2; i >= 0; i--){
+        if(nums[i] == nums[i+1]){
             nums.splice(i,1)
-        }else{
-            judge[nums[i]+100] = true;
-            count++;
         }
     }
-    return count;
+    return nums.length;
 };
