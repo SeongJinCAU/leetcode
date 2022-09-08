fea0cac81dbe3ebb51ac8568a570105ff76d9435
Choi Seong Jin <39955801+remope@users.noreply.github.com>
2022-09-08 21:00:09 +0900

    Time: 99 ms (78.21%), Space: 45.5 MB (73.60%) - LeetHub

--- 136-single-number/136-single-number.ts
+++ 136-single-number/136-single-number.ts
@@ -1,9 +1,10 @@
 function singleNumber(nums: number[]): number {
-    if(nums.length === 1){
+    let len : number = nums.length;
+    if(len === 1){
         return nums[0];
     }
-    for(let i : number = 1; i < nums.length; i++){
+    for(let i : number = 1; i < len; i++){
         nums[i] = nums[i-1]^nums[i];
     }
-    return nums[nums.length - 1];
+    return nums[len - 1];
 };
