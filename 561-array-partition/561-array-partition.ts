c3aed612e89c6cee0debfe384af8bc4930fa15ee
Choi Seong Jin <39955801+remope@users.noreply.github.com>
2022-09-16 14:04:44 +0900

    Time: 235 ms (18.57%), Space: 48.1 MB (92.86%) - LeetHub

--- 561-array-partition/561-array-partition.ts
+++ 561-array-partition/561-array-partition.ts
@@ -1,22 +1,13 @@
 function arrayPairSum(nums: number[]): number {
-    let sumLen: number = nums.length/2;
+    let length: number = nums.length;
     let pairSum: number = 0;
-    let min: number = 0;
-    let max: number = 0;
-    for(let i: number = 0; i < sumLen; i++){
-        if(i % 2 === 0){
-            min = Math.min.apply(null, nums);
-            pairSum += min;
-            nums.splice(nums.indexOf(min), 1);
-            min = Math.min.apply(null, nums);
-            nums.splice(nums.indexOf(min), 1);  
-        }else{
-            max = Math.max.apply(null, nums);
-            nums.splice(nums.indexOf(max), 1);  
-            max = Math.max.apply(null, nums);
-            pairSum += max;
-            nums.splice(nums.indexOf(max), 1);  
-        }   
+    nums = nums.sort(function(a, b)  {
+        if(a > b) return 1;
+        if(a === b) return 0;
+        if(a < b) return -1;
+    });
+    for(let i: number = 0; i<length; i = i + 2){
+      pairSum += nums[i];
     }
     return pairSum;
 };
